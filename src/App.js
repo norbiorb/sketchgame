1dd485b6a47a278126186b0e710ed9854aaca108
norbiorb <norbiorb@gmail.com>
2020-03-18 17:21:25 +0100

    Add project starter files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getPrediction } from "./helpers.js";
 
-function App() {
+function Controls({ theCanvas, model, labels }) {
+  let [prediction, setPrediction] = useState(""); // Sets default label to empty string.
+
+  useEffect(() => {
+    console.log(prediction);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          const canvas = theCanvas.current;
+          const ctx = canvas.getContext("2d");
+          ctx.fillRect(0, 0, canvas.height, canvas.width);
+        }}
+      >
+        Clear the canvas.
+      </button>
+      <button
+        onClick={() =>
+          getPrediction(theCanvas, model).then(prediction =>
+            setPrediction(labels[prediction[0]])
+          )
+        }
+      >
+        Predict the drawing.
+      </button>
     </div>
   );
 }
 
-export default App;
+const Canvas = React.forwardRef((props, ref) => {
+  let mouseDown = false;
+  let lastX;
+  let lastY;
+
+  function drawLine(canvas, x, y, lastX, lastY) {
+    let context = canvas.getContext("2d");
+
+    context.strokeStyle = "#000000";
+    context.lineWidth = 12;
+    context.lineJoin = "round";
+
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+    context.lineTo(x, y);
+    context.closePath();
+    context.stroke();
+
+    return [x, y];
+  }
+
+  const handleMouseup = () => {
+    mouseDown = false;
+    [lastX, lastY] = [undefined, undefined];
+  };
+
+  const handleMousemove = e => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    if (mouseDown) {
+      [lastX, lastY] = drawLine(e.target, x, y, lastX, lastY);
+    }
+  };
+
+  useEffect(() => {
+    const canvas = ref.current;
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, canvas.height, canvas.width);
+  });
+
+  return (
+    <canvas
+      height={300}
+      width={300}
+      ref={ref}
+      onMouseDown={() => (mouseDown = true)}
+      onMouseUp={handleMouseup}
+      onMouseMove={e => handleMousemove(e)}
+    />
+  );
+});
+
+export { Canvas, Controls };
