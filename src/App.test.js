0573acf1f57bb8f81ea34fc263a492544d5ec39b
norbiorb <norbiorb@gmail.com>
2020-04-06 10:53:25 +0200

    Add App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it ('should render a Game button', () => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+      const links = document.querySelectorAll('a');
+      expect(links.length).toBe(3);
+      expect(links[2].getAttribute('href')).toBe('/game');
+  });
 });
