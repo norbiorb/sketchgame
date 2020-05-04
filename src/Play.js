bb95335831b8dd142c9fd4b087f2d809d3f33e48
norbiorb <norbiorb@gmail.com>
2020-05-04 12:36:48 +0200

    Add src/Play.js file

---
+++ src/Play.js
@@ -0,0 +1,162 @@
+import React, { createContext, useEffect, useState, useContext, useRef } from "react";
+import { Link } from "react-router-dom";
+
+import { GameContext } from './Game';
+
+import { getPrediction } from "./helpers.js";
+import { useRounds } from './Round'; 
+import { Canvas } from './Canvas'; 
+import { Controls } from './Controls';
+
+const PlayContext = createContext({});
+
+const Play = () => { 
+    const { 
+        toggleGameEnded, 
+        labels, 
+        indices, 
+        dispatchPoints, 
+        model, 
+        secondsPerRound, 
+        ref, 
+        STORE_RESULT, 
+        CORRECT, 
+        TIMEDOUT 
+    } = useContext(GameContext);
+  
+    const timerRef = useRef();
+  
+    const reduceRoundState = (state, action) => {
+      const { type, payload } = action;
+      switch (type) {
+        case STORE_RESULT: 
+          let newState = {...state, 
+            label: payload.label, 
+            result: payload.result,
+            timeUsed: payload.timeUsed
+            };
+            console.log('reduceRoundState ', newState);
+            return newState;
+        default:
+          return state;
+      }
+    }
+  
+    const initialRoundState = {
+      label: '',
+      result: '',
+      timeUsed: secondsPerRound,
+      points: 0
+    };
+  
+    const [rounds, activeRound, dispatchActiveRound, roundState, dispatchRoundState] 
+            = useRounds(labels, reduceRoundState, initialRoundState);
+  
+    useEffect(() => {
+      if (activeRound === labels.length) {
+        toggleGameEnded();
+      }
+    }, [activeRound, toggleGameEnded, labels]);
+  
+    let round = rounds[activeRound];
+  
+    const [result, setResult] = useState('');
+  
+    const clearCanvas = (ref) => {
+      const canvas = ref.current;
+      const ctx = canvas.getContext("2d");
+      ctx.fillRect(0, 0, canvas.height, canvas.width);
+    }
+  
+    const getTime = (timerRef) => {
+      return timerRef.current.innerHTML;
+    }
+  
+    const handleTimeout = (timeleft) => {
+      if (timeleft <= 0) {
+        clearCanvas(ref);
+        setResult(TIMEDOUT);
+        dispatchActiveRound({ type: 'increment' });
+        dispatchRoundState({ 
+          type: STORE_RESULT,
+          payload: {
+            label: labels[activeRound],
+            result: TIMEDOUT,
+            timeUsed: secondsPerRound
+          }
+        });
+      }
+    }
+  
+    const handlePrediction = () => {
+      getPrediction(ref, model, timerRef).then((prediction) => {   
+        const label = labels[activeRound];
+  
+         // use permutation indices to get the correct label
+        const predictedLabel = labels[indices[prediction[0]]];
+       
+        if (label === predictedLabel) {
+          const timeUsed = secondsPerRound - parseInt(getTime(timerRef));
+          console.log('Time Used', timeUsed)
+          setResult(CORRECT);
+          dispatchPoints({type: 'increment', 
+            payload: {
+              timeUsed: timeUsed,
+            }
+          });
+          clearCanvas(ref);
+          dispatchActiveRound({ type: 'increment' });
+    
+          dispatchRoundState({
+            type: STORE_RESULT,
+            payload: {
+              label: labels[activeRound],
+              result: CORRECT,
+              timeUsed: timeUsed
+            }
+          });
+  
+        } else {
+          console.log('canvas prediction wrong: ', predictedLabel);
+        }
+      });
+    };
+  
+    return (
+      <div className="nes-container with-title is-dark">
+      <h2 className="title">Sketch Round {activeRound +1} of {labels.length} Rounds</h2> 
+        <header className="header">
+          <Link className=" nes-btn" to="/">Home</Link> 
+        </header> 
+        <PlayContext.Provider value={{
+              activeRound,
+              dispatchActiveRound,
+              roundState,
+              dispatchRoundState,
+              result,
+              setResult,
+              timerRef,
+              handlePrediction,
+              handleTimeout
+        }}>   
+          <main className="main">         
+            <Canvas />
+            <div className="status">
+            {round}   
+            <Controls />    
+            </div>     
+          </main> 
+        <button
+          className="nes-btn is-warning"
+          onClick={() => {
+            dispatchActiveRound({ type: 'increment' });
+          }}
+        >
+          Next Round
+        </button>
+        </PlayContext.Provider>
+      </div>      
+    ) 
+  }
+
+  export { Play, PlayContext }
