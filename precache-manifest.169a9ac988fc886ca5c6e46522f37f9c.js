955a4946f8b8ab3e102d063ff8c2a222e700b5b7
norbiorb <norbiorb@gmail.com>
2020-05-11 19:26:38 +0200

    Updates

---
+++ precache-manifest.169a9ac988fc886ca5c6e46522f37f9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518caf8e6140aa65b6b65954cff77046",
+    "url": "/project-react-c1-s8-3204-1657/index.html"
+  },
+  {
+    "revision": "930404a7ce1c939edb71",
+    "url": "/project-react-c1-s8-3204-1657/static/css/2.bc82e399.chunk.css"
+  },
+  {
+    "revision": "0fe71611e4dd31e6be43",
+    "url": "/project-react-c1-s8-3204-1657/static/css/main.717e10bb.chunk.css"
+  },
+  {
+    "revision": "930404a7ce1c939edb71",
+    "url": "/project-react-c1-s8-3204-1657/static/js/2.5a68f7f5.chunk.js"
+  },
+  {
+    "revision": "2af94bd0a3d5d7e3a1af920585139e62",
+    "url": "/project-react-c1-s8-3204-1657/static/js/2.5a68f7f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe71611e4dd31e6be43",
+    "url": "/project-react-c1-s8-3204-1657/static/js/main.a7f5bcdc.chunk.js"
+  },
+  {
+    "revision": "e05abb24995d98b7cc2b",
+    "url": "/project-react-c1-s8-3204-1657/static/js/runtime-main.a70a530d.js"
+  }
+]);
